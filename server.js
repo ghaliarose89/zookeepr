13ee62de0af6b88ae42d0cdd791c90d6b8f736ef
unknown <ghaliarose89@gmail.com>
2021-10-25 11:13:12 -0500

    req.param

--- server.js
+++ server.js
@@ -39,7 +39,12 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-  }
+  };
+  function findById(id,animalsArray){
+    const result = animalsArray.filter(animal=>animal.id === id)[0];
+    return result;
+
+  };
   
 app.get('/api/animals', (req, res) => {
     let result = animals;
@@ -49,4 +54,11 @@ app.get('/api/animals', (req, res) => {
 });
 app.listen(PORT, () => {
     console.log(`API server now on port 3001!${PORT}`);
+});
+
+app.get ('/api/animals/:id', (req,res)=>{
+  const result = findById (req.params.id,animals);
+  if (result){
+  res.json(result);}
+  else res.send(404);
 })
